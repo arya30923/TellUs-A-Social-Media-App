87ae2bd345d045f15067cc9d3de4a08373d02b52
Arya Singh <arya30923@gmail.com>
2023-10-26 15:41:17 +0530

    .

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -13,7 +13,7 @@ async function Page({ params }: { params: { id: string } }) {
     if (!user) return null;
 
     const userInfo = await fetchUser(params.id);
-   console.log({userInfo})
+//    console.log({userInfo})
     if (!userInfo?.onboarded) redirect("/onboarding");
 
     return (
