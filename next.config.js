a3b12a123deb1b863577c381922dc7c306676b2d
Arya Singh <arya30923@gmail.com>
2023-10-26 15:27:35 +0530

    utfs hostname added

--- next.config.js
+++ next.config.js
@@ -1,35 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
   },
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-      typescript: {
-        ignoreBuildErrors: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-    },
-  };
-  
-  module.exports = nextConfig
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
